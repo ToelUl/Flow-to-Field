6ab0ba45a2963ff8538d07c305abe176c5e157de
ray <ray100182181235@gmail.com>
2025-09-03 20:35:07 +0800

    remove unused imports from __init__.py

--- phyflow/phy_flow_matching/models/__init__.py
+++ phyflow/phy_flow_matching/models/__init__.py
@@ -1,14 +1,8 @@
-from .di_res_unet import DiResUnet
-from .rodit_unet import RoDitUnet
-from .flux_unet import FluxUNet
 from .flow_unet import FlowUNet
 from .flexible_dit import FlexibleDiT
 from .model_wrapper import CFMWrapper
 
 __all__ = [
-    'DiResUnet',
-    'RoDitUnet',
-    'FluxUNet',
     'FlowUNet',
     'FlexibleDiT',
     'CFMWrapper',
